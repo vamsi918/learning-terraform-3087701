6909544258efb662cd1e69bf9cd6a83aefe321c1
vamsi918 <52372006+vamsi918@users.noreply.github.com>
2024-09-27 16:37:34 +1000

    Create SG Group Fixed errors

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" { 
-  type       = "ingress
+  type       = "ingress"
   from_port  = 80
   to_port    = 80
   protocol   = "tcp" 
@@ -47,7 +47,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 }
 
 resource "aws_security_group_rule" "blog_https_in" { 
-  type       = "ingress
+  type       = "ingress"
   from_port  = 443
   to_port    = 443
   protocol   = "tcp" 
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 }
 
 resource "aws_security_group_rule" "blog_everything_out" { 
-  type       = "egress
+  type       = "egress"
   from_port  = 0
   to_port    = 0
   protocol   = "-1" 
