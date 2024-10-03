9a4c7b45639cb5e1816fa21f0d4a012b2405ec59
vamsi918 <52372006+vamsi918@users.noreply.github.com>
2024-10-03 20:25:46 +1000

    rf_fe4

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "${var.environment.name}-
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
