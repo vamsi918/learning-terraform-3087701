24b47dea83774a013335a9846c3d8431258501a2
vamsi918 <52372006+vamsi918@users.noreply.github.com>
2024-10-03 20:18:48 +1000

    RefactorVariable_fe2

--- variables.tf
+++ variables.tf
@@ -11,9 +11,8 @@ variable "ami_filter" {
     owner = string
   })
 
-  default= {
-  
-    name = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  default= {  
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
     owner = "979382823631" # Bitnami
   }
 }
