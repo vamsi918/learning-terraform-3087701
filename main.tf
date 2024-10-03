bac3242fac013e974115b3d182703f92d878834c
vamsi918 <52372006+vamsi918@users.noreply.github.com>
2024-10-03 19:04:43 +1000

    autoscaling

--- main.tf
+++ main.tf
@@ -48,7 +48,25 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "autoscaling" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "7.0.0"
+
+  name = "blog"
+  min_size =1 
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+  
+  image_id   = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
+
+}
+
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
