0152f26457bafb5cd961b42b35e987c5ed86df81
vamsi918 <52372006+vamsi918@users.noreply.github.com>
2024-10-03 20:16:57 +1000

    RefactorVariable_Fe

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "instance_type" {
   default     = "t3.nano"
 }
 
-variable "ami_filter"
+variable "ami_filter" {
   description = "Name filter and owner for AMI"
   
   type = object({
@@ -16,7 +16,7 @@ variable "ami_filter"
     name = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
     owner = "979382823631" # Bitnami
   }
-
+}
 variable "environment" {
   description = "Development Environment"
   
@@ -34,10 +34,10 @@ variable "environment" {
 
 variable "asg_min_size" { 
   description = "Minimum number of instances in the ASG"
-  default = 1 
+  default     = 1 
  }
 
  variable "asg_max_size" { 
   description = "Maximum number of instances in the ASG"
-  default = 2
+  default     = 2
  }
